e2d7a4da8bf5cab06316ea4b649e3d72359473e6
jsgalvish <jsgalvish@unal.edu.co>
2019-09-30 21:54:50 -0500

    0x05 - Tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        prod = a / b
+    except ZeroDivisionError:
+        prod = None
+    finally:
+        print("Inside result: {}".format(prod))
+    return prod
